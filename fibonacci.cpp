312296d424b3d12b69e176f3c140f43816194d0f
limoze0327 <81509483+limoze0327@users.noreply.github.com>
2021-08-04 17:23:31 +0900

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+long long a[91]; // 90번째 계산했을 때 값이 너무 커서 int 대신 long long 으로 수정 
+
+
+long long fibonacci(int n) {
+    a[0] = 0;
+    a[1] = 1;
+    for (int i = 0; i < n; i++) {
+        a[i + 2] = a[i + 1] + a[i];
+    }
+    return a[n];
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::cout << fibonacci(n) << "\n";
+
+}
